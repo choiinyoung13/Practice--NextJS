50b39e2e8d143d94cbe81a4a310e7661a82c8cb4
chldls153@naver.com <chldls153@gmail.com>
2025-11-08 16:06:12 +0900

    기존 페이지 구축 및 tailwind css 작업

---
+++ using-app-router/components/main-header/main-header.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import logoImg from '@/assets/logo.png'
+import MainHeaderBackground from './main-header-background'
+import NavLink from './nav-link'
+import Link from 'next/link'
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className="flex justify-between items-center py-8 px-4 md:px-[10%]">
+        <div className="flex items-center gap-8">
+          <Image
+            className="w-20 h-20 object-contain filter drop-shadow-[0_0_0.75rem_rgba(0,0,0,0.5)]"
+            src={logoImg}
+            alt="A plate with food on it"
+            priority
+          />
+          <Link
+            href={'/'}
+            className="text-[#ddd6cb] font-bold  font-[Montserrat] text-[1.5rem]"
+          >
+            NEXTLEVEL FOOD
+          </Link>
+        </div>
+
+        <div>
+          <NavLink href={'/meals'}>Bowse Meals</NavLink>
+          <NavLink href={'/community'}>Foodies Community</NavLink>
+        </div>
+      </header>
+    </>
+  )
+}
---
+++ using-app-router/components/main-header/nav-link.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+interface navLinkProps {
+  href: string
+  children: ReactNode
+}
+
+export default function NavLink({ href, children }: navLinkProps) {
+  const path = usePathname()
+  const isActive = path.startsWith(href)
+
+  console.log('gofsd')
+
+  return (
+    <Link
+      className={`font-bold py-2 px-4 transition-colors duration-300 hover:text-[#ff8a05] ${
+        isActive ? 'text-[#f9b331]' : 'text-[#ddd6cb]'
+      }`}
+      href={href}
+    >
+      {children}
+    </Link>
+  )
+}
