5a4f73851ad41cb5c1c5c0358bff6ddc40486249
chldls153@naver.com <chldls153@gmail.com>
2025-11-25 19:24:54 +0900

    feat: 앱 스타일링 및 더미 데이터 사용

---
+++ deep-dive/app/news/page.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { DUMMY_NEWS } from '@/dummy-news'
+
+export default function NewsPage() {
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map(news => (
+          <li>
+            <Link href={`/news/${news.slug}`}>
+              <img src={`/images/news/${news.image}`} alt={news.title} />
+              {news.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
